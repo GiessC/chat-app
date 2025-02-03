3ada6db7ac4b58748c5d829d00ec461afe75597d
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-02 19:59:10 -0500

    made server attributes private and added constructor

--- api/src/features/server/entities/server.entity.ts
+++ api/src/features/server/entities/server.entity.ts
@@ -1,5 +1,14 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export class Server {
-  serverId: string;
-  ownerId: string;
-  name: string;
+  private serverId: string;
+  private ownerId: string;
+  private name: string;
+
+  constructor(ownerId: string, name: string, serverId?: string) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    this.serverId = serverId ?? (uuidv4() as string);
+    this.ownerId = ownerId;
+    this.name = name;
+  }
 }
