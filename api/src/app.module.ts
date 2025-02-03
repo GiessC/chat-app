4c91c1ecc1be0d572ad0330ee338294353a6ccd0
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-02 20:47:27 -0500

    added nestjs configuration

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration, { configSchema } from './config/configuration';
+import { ZodError } from 'zod';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+function validate(config: Record<string, unknown>): Record<string, unknown> {
+  try {
+    return configSchema.parse(config);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      throw new Error(
+        `Configuration validation errors: ${JSON.stringify(error.issues)}`,
+      );
+    }
+    throw error;
+  }
+}
