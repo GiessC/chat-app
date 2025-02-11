aa7478c8fea979fb18903633231f1d36b86c4cbb
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-10 23:48:25 -0500

    added repository error handling

--- api/src/features/server/providers/server.dynamo.repository.ts
+++ api/src/features/server/providers/server.dynamo.repository.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import ServerDynamoDbDto from '../dto/server.dynamo.dto';
 import { Server } from '../entities/server.entity';
 import DynamoDbService from '../../../database/dynamo-db.service';
+import { ConditionalCheckFailedException } from '@aws-sdk/client-dynamodb';
 
 @Injectable()
 export class ServerDynamoDbRepository {
@@ -12,18 +13,33 @@ export class ServerDynamoDbRepository {
   ) {}
 
   public async create(server: Server): Promise<Server> {
-    console.log('Creating server in DynamoDB:', server.serverId);
-    const dto = new ServerDynamoDbDto(server);
-    await this.dynamoDb.save<ServerDynamoDbDto>({
-      TableName: this.configService.get<string>('DYNAMODB_TABLE_NAME'),
-      Item: dto,
-      ExpressionAttributeNames: {
-        '#pk': 'pk',
-        '#sk': 'sk',
-      },
-      ConditionExpression:
-        'attribute_not_exists(#pk) AND attribute_not_exists(#sk)',
-    });
-    return server;
+    try {
+      console.log('Creating server in DynamoDB:', server.serverId);
+      const dto = new ServerDynamoDbDto(server);
+      await this.dynamoDb.save<ServerDynamoDbDto>({
+        TableName: this.configService.get<string>('DYNAMODB_TABLE_NAME'),
+        Item: dto,
+        ExpressionAttributeNames: {
+          '#pk': 'pk',
+          '#sk': 'sk',
+        },
+        ConditionExpression:
+          'attribute_not_exists(#pk) AND attribute_not_exists(#sk)',
+      });
+      return server;
+    } catch (error: unknown) {
+      console.error(error);
+      if (error instanceof ConditionalCheckFailedException) {
+        throw new ServerDynamoDbRepositoryError('Server already exists');
+      }
+      throw new ServerDynamoDbRepositoryError('Failed to create server');
+    }
+  }
+}
+
+export class ServerDynamoDbRepositoryError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'ServerDynamoDbRepositoryError';
   }
 }
