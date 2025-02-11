25b0d97700986bd689c590b382c8caec073a1484
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-10 22:55:59 -0500

    added static pk and sk generator methods

--- api/src/features/server/dto/server.dynamo.dto.ts
+++ api/src/features/server/dto/server.dynamo.dto.ts
@@ -9,11 +9,19 @@ export default class ServerDynamoDbDto {
   readonly createdAt: string;
 
   constructor(server: Server) {
-    this.pk = `SERVER#${server.serverId}`;
-    this.sk = `SERVER#${server.serverId}`;
+    this.pk = ServerDynamoDbDto.generatePk(server.serverId);
+    this.sk = ServerDynamoDbDto.generateSk(server.serverId);
     this.serverId = server.serverId;
     this.ownerId = server.ownerId;
     this.name = server.name;
     this.createdAt = server.createdAt;
+  }
+
+  public static generatePk(serverId: string) {
+    return `SERVER#${serverId}`;
+  }
+
+  public static generateSk(serverId: string) {
+    return `SERVER#${serverId}`;
   }
 }
