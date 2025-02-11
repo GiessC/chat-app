a13f1d313cd116ef49507c309fef1cca5f017f98
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-10 23:24:46 -0500

    converted server repository to use dynamodb service

--- api/src/features/server/providers/server.dynamo.repository.ts
+++ api/src/features/server/providers/server.dynamo.repository.ts
@@ -1,54 +1,29 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import ServerDynamoDbDto from '../dto/server.dynamo.dto';
 import { Server } from '../entities/server.entity';
+import DynamoDbService from '../../../database/dynamo-db.service';
 
 @Injectable()
 export class ServerDynamoDbRepository {
-  private readonly dynamoDb: DynamoDBDocumentClient;
-
-  constructor(private readonly configService: ConfigService) {
-    this.dynamoDb = DynamoDBDocumentClient.from(
-      new DynamoDBClient({
-        region: this.configService.get<string>('AWS_REGION'),
-      }),
-    );
-  }
+  constructor(
+    private readonly dynamoDb: DynamoDbService,
+    private readonly configService: ConfigService,
+  ) {}
 
   public async create(server: Server): Promise<Server> {
     console.log('Creating server in DynamoDB:', server.serverId);
     const dto = new ServerDynamoDbDto(server);
-    const response = await this.dynamoDb.send(
-      new PutCommand({
-        TableName: this.configService.get<string>('DYNAMODB_TABLE_NAME'),
-        Item: dto,
-        ExpressionAttributeNames: {
-          '#pk': 'pk',
-          '#sk': 'sk',
-        },
-        ConditionExpression:
-          'attribute_not_exists(#pk) AND attribute_not_exists(#sk)',
-      }),
-    );
-    console.log(`Received response from DynamoDB:`, response);
-    if (response.$metadata.httpStatusCode !== HttpStatus.OK) {
-      console.error(
-        'DynamoDB: Server creation failed with HTTP status:',
-        response.$metadata.httpStatusCode,
-        '. Full response:',
-        response,
-      );
-      throw new ServerRepositoryError('Server creation failed.');
-    }
+    await this.dynamoDb.save<ServerDynamoDbDto>({
+      TableName: this.configService.get<string>('DYNAMODB_TABLE_NAME'),
+      Item: dto,
+      ExpressionAttributeNames: {
+        '#pk': 'pk',
+        '#sk': 'sk',
+      },
+      ConditionExpression:
+        'attribute_not_exists(#pk) AND attribute_not_exists(#sk)',
+    });
     return server;
-  }
-}
-
-class ServerRepositoryError extends Error {
-  public constructor(message: string) {
-    super(message);
-    this.name = 'ServerRepositoryError';
   }
 }
--- api/src/features/server/server.module.ts
+++ api/src/features/server/server.module.ts
@@ -3,9 +3,10 @@ import { ServerService } from './providers/server.service';
 import { ServerController } from './server.controller';
 import { ConfigModule } from '@nestjs/config';
 import { ServerDynamoDbRepository } from './providers/server.dynamo.repository';
+import { DynamoDbModule } from '../../database/dynamo-db.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, DynamoDbModule],
   controllers: [ServerController],
   providers: [ServerService, ServerDynamoDbRepository],
 })
