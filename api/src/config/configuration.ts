cd759193607cdc531270b58a1c5eab93a6d7ff12
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-10 21:33:46 -0500

    made configuration nested

--- api/src/config/configuration.ts
+++ api/src/config/configuration.ts
@@ -3,19 +3,11 @@
 import { z } from 'zod';
 
 export const configSchema = z.object({
-  database: z.object({
-    dynamoDb: z.object({
-      dynamoDbMetadataTable: z.string(),
-    }),
-  }),
+  dynamoDbMetadataTable: z.string(),
 });
 
 export type Configuration = z.infer<typeof configSchema>;
 
 export default () => ({
-  database: {
-    dynamoDb: {
-      dynamoDbMetadataTable: process.env.DYNAMODB_METADATA_TABLE,
-    },
-  },
+  dynamoDbMetadataTable: process.env.DYNAMODB_METADATA_TABLE,
 });
