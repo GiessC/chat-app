52ff6a34abae10048423e41e2693452b83cee363
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-10 22:46:08 -0500

    added endpoint response handling

--- api/src/features/server/server.controller.ts
+++ api/src/features/server/server.controller.ts
@@ -1,16 +1,37 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { ServerService } from './server.service';
 import { CreateServerDto } from './dto/create-server.dto';
+import ApiResponse, { ApiResponseWithError } from '../../common/ApiResponse';
+import ServerResponseDto from './dto/server-response.dto';
 
 @Controller('server')
 export class ServerController {
   constructor(private readonly serverService: ServerService) {}
 
   @Post()
-  create(@Body() createServerDto: CreateServerDto) {
-    return this.serverService.create(
-      createServerDto.ownerId,
-      createServerDto.name,
-    );
+  public async create(
+    @Body() createServerDto: CreateServerDto,
+  ): Promise<ApiResponse<ServerResponseDto>> {
+    try {
+      const server = await this.serverService.create(
+        createServerDto.ownerId,
+        createServerDto.name,
+      );
+      const serverResponseDto: ServerResponseDto = {
+        serverId: server.serverId,
+        ownerId: server.ownerId,
+        name: server.name,
+        createdAt: server.createdAt,
+      };
+      return new ApiResponse<ServerResponseDto>(
+        `Created server '${createServerDto.name}'`,
+        serverResponseDto,
+      );
+    } catch (error: unknown) {
+      console.error(`Server controller error caused by: ${String(error)}`);
+      return new ApiResponseWithError<ServerResponseDto>(
+        'Failed to create server.',
+      );
+    }
   }
 }
