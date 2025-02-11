f0980a2b6beb3287bd0e3393bc6924cb42c210f5
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-10 21:41:59 -0500

    added env files to app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,31 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import configuration, { configSchema } from './config/configuration';
-import { ZodError } from 'zod';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [configuration],
-      validate,
+      envFilePath: [
+        '.env.local',
+        '.env.development.local',
+        '.env.development',
+        '.env.production.local',
+        '.env.production',
+      ],
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
-function validate(config: Record<string, unknown>): Record<string, unknown> {
-  try {
-    return configSchema.parse(config);
-  } catch (error) {
-    if (error instanceof ZodError) {
-      throw new Error(
-        `Configuration validation errors: ${error.issues.map((issue) => `${issue.path as unknown as string} - ${issue.message}`).join(', ')}`,
-      );
-    }
-    throw error;
-  }
-}
