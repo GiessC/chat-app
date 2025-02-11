449ef46c19e5db76ed64c038ef8b9bf198d25216
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-10 21:37:56 -0500

    imported ConfigModule in server module

--- api/src/features/server/server.module.ts
+++ api/src/features/server/server.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ServerService } from './server.service';
 import { ServerController } from './server.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [ServerController],
   providers: [ServerService],
 })
