9135cee3e5b8d5f663ad272b4e78693c7fb0336a
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-10 21:37:44 -0500

    added service create implementation using server repository

--- api/src/features/server/server.service.ts
+++ api/src/features/server/server.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateServerDto } from './dto/create-server.dto';
+import { ServerDynamoDbRepository } from './server-dynamo.repository';
 
 @Injectable()
 export class ServerService {
-  create(createServerDto: CreateServerDto) {
-    return 'This action adds a new server';
+  constructor(private readonly repository: ServerDynamoDbRepository) {}
+
+  public async create(createServerDto: CreateServerDto) {
+    return await this.repository.create(createServerDto);
   }
 }
