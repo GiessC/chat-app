bc66f83fcaff02a959c4a31dd67c6eb7382ab154
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-10 21:35:52 -0500

    added getters for applicable fields on Server

--- api/src/features/server/entities/server.entity.ts
+++ api/src/features/server/entities/server.entity.ts
@@ -1,13 +1,28 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export class Server {
-  private serverId: string;
-  private ownerId: string;
-  private name: string;
+  private readonly _serverId: string;
+  private readonly _ownerId: string;
+  private readonly _name: string;
+  private readonly _createdAt: string;
 
-  constructor(ownerId: string, name: string, serverId?: string) {
-    this.serverId = serverId ?? uuidv4();
-    this.ownerId = ownerId;
-    this.name = name;
+  constructor(
+    ownerId: string,
+    name: string,
+    serverId?: string,
+    createdAt?: string,
+  ) {
+    this._serverId = serverId ?? uuidv4();
+    this._ownerId = ownerId;
+    this._name = name;
+    this._createdAt = createdAt ?? new Date().toISOString();
+  }
+
+  get serverId(): string {
+    return this._serverId;
+  }
+
+  get ownerId(): string {
+    return this._ownerId;
   }
 }
