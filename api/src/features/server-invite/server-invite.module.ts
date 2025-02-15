0334cdee775305acf0369c9f6a7ff08cac165b47
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-15 16:17:48 -0500

    added server invite controller to module

---
+++ api/src/features/server-invite/server-invite.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DynamoDbModule } from '../../database/dynamo-db.module';
+import ServerInviteService from './providers/server-invite.service';
+import { ServerInviteDynamoRepository } from './providers/server-invite.dynamo.repository';
+import { ServerInviteController } from './server-invite.controller';
+
+@Module({
+  imports: [ConfigModule, DynamoDbModule],
+  controllers: [ServerInviteController],
+  providers: [ServerInviteService, ServerInviteDynamoRepository],
+})
+export class ServerModule {}
