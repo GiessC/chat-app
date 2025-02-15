23a04b74a0759015e8f69d483a4041a916d44adf
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-15 18:51:14 -0500

    moved server member into server

--- api/src/features/server-member/server-member.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ServerMemberDynamoDbRepository } from './providers/server-member.dynamo.repository';
-
-@Controller('server-member')
-export class ServerMemberController {
-  constructor(
-    private readonly serverMemberRepo: ServerMemberDynamoDbRepository,
-  ) {}
-}
