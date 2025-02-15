880735106dde6bbbc1422589e9922518869c749f
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-15 18:50:31 -0500

    removed error handling from server controller

--- api/src/features/server/server.controller.ts
+++ api/src/features/server/server.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { ServerService } from './providers/server.service';
 import { CreateServerDto } from './dto/create-server.dto';
-import ApiResponse, {
-  ApiResponseWithError,
-  ErrorCode,
-} from '../../common/ApiResponse';
+import ApiResponse from '../../common/ApiResponse';
 import ServerResponseDto from './dto/server-response.dto';
 
 @Controller('server')
@@ -15,27 +12,19 @@ export class ServerController {
   public async create(
     @Body() createServerDto: CreateServerDto,
   ): Promise<ApiResponse<ServerResponseDto>> {
-    try {
-      const server = await this.serverService.create(
-        createServerDto.ownerId,
-        createServerDto.name,
-      );
-      const serverResponseDto: ServerResponseDto = {
-        serverId: server.serverId,
-        ownerId: server.ownerId,
-        name: server.name,
-        createdAt: server.createdAt,
-      };
-      return new ApiResponse<ServerResponseDto>(
-        `Created server '${createServerDto.name}'`,
-        serverResponseDto,
-      );
-    } catch (error: unknown) {
-      console.error(`Server controller error caused by: ${String(error)}`);
-      return new ApiResponseWithError<ServerResponseDto>(
-        'Failed to create server.',
-        ErrorCode.InternalError,
-      );
-    }
+    const server = await this.serverService.create(
+      createServerDto.ownerId,
+      createServerDto.name,
+    );
+    const serverResponseDto: ServerResponseDto = {
+      serverId: server.serverId,
+      ownerId: server.ownerId,
+      name: server.name,
+      createdAt: server.createdAt,
+    };
+    return new ApiResponse<ServerResponseDto>(
+      `Created server '${createServerDto.name}'`,
+      serverResponseDto,
+    );
   }
 }
