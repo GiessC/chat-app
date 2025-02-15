de073cc89f54073c896e881bed58608f334fc489
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-15 16:16:55 -0500

    added server invite controller

---
+++ api/src/features/server-invite/server-invite.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import ApiResponse, {
+  ApiResponseWithError,
+  ErrorCode,
+} from '../../common/ApiResponse';
+import ServerInviteService from './providers/server-invite.service';
+import CreateServerInviteDto from './dto/create-server-invite.dto';
+import ServerInviteResponseDto from './dto/server-invite-response.dto';
+
+@Controller(['server', 'invite'])
+export class ServerInviteController {
+  constructor(private readonly serverInviteService: ServerInviteService) {}
+
+  @Post()
+  public async create(
+    @Body() createServerInviteDto: CreateServerInviteDto,
+  ): Promise<ApiResponse<ServerInviteResponseDto>> {
+    try {
+      const serverInvite = await this.serverInviteService.create(
+        createServerInviteDto.serverId,
+        createServerInviteDto.creatorId,
+        createServerInviteDto.expirationDate,
+        createServerInviteDto.maxUses,
+      );
+      const responseDto: ServerInviteResponseDto = {
+        inviteCode: serverInvite.getInviteCode(),
+      };
+      return new ApiResponse<ServerInviteResponseDto>(
+        'Created server invite',
+        responseDto,
+      );
+    } catch (error: unknown) {
+      console.error(
+        `Server invite controller error caused by: ${String(error)}`,
+      );
+      return new ApiResponseWithError<ServerInviteResponseDto>(
+        'Failed to create server invite.',
+        ErrorCode.InternalError,
+      );
+    }
+  }
+}
