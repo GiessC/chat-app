ac4d2d867821923c25c87e834a88d4782052fa09
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-15 16:10:52 -0500

    create dto modified to represent expected request

--- api/src/features/server-invite/dto/create-server-invite.dto.ts
+++ api/src/features/server-invite/dto/create-server-invite.dto.ts
@@ -1,10 +1,8 @@
 export default class CreateServerInviteDto {
   constructor(
-    public readonly inviteId: string,
     public readonly serverId: string,
     public readonly creatorId: string,
-    public readonly expirationDate: Date,
-    public readonly maxUses: number,
-    public readonly uses: number,
+    public readonly expirationDate?: Date,
+    public readonly maxUses?: number,
   ) {}
 }
