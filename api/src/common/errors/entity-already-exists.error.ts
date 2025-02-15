f6ea2c66bcff80047e3bcbe82ef18c2b87826f40
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-15 17:20:22 -0500

    added error code to entity already exists

--- api/src/common/errors/entity-already-exists.error.ts
+++ api/src/common/errors/entity-already-exists.error.ts
@@ -1,8 +1,15 @@
 import BaseError from './base.error';
 import { HttpStatus } from '@nestjs/common';
+import { ErrorCode } from './error-code';
 
 export default class EntityAlreadyExistsError extends BaseError {
   constructor(message: string) {
-    super('EntityAlreadyExistsError', message, undefined, HttpStatus.CONFLICT);
+    super(
+      'EntityAlreadyExistsError',
+      message,
+      ErrorCode.ENTITY_ALREADY_EXISTS,
+      undefined,
+      HttpStatus.CONFLICT,
+    );
   }
 }
