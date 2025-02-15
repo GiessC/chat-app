00ee24054a6ef9a355c7fba54d0a663c5bc19c81
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-15 16:07:02 -0500

    improved server invite constructor

--- api/src/features/server-invite/entities/server-invite.entity.ts
+++ api/src/features/server-invite/entities/server-invite.entity.ts
@@ -1,12 +1,18 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default class ServerInvite {
+  private readonly _uses: number = 0;
+  private readonly _inviteId: string;
+
   constructor(
-    private readonly _inviteId: string,
     private readonly _serverId: string,
     private readonly _creatorId: string,
     private readonly _expirationDate: Date,
     private readonly _maxUses: number,
-    private readonly _uses: number,
-  ) {}
+    inviteId?: string,
+  ) {
+    this._inviteId = inviteId ?? uuidv4();
+  }
 
   public static decodeLink(link: string) {
     const [inviteId, serverId] = Buffer.from(link, 'base64')
