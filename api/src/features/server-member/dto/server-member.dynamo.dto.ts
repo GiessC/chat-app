66ae724360b3f58cef0f7fdd79725e85f3461919
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-15 16:58:52 -0500

    created server member dynamo dto

---
+++ api/src/features/server-member/dto/server-member.dynamo.dto.ts
@@ -0,0 +1,98 @@
+export default class ServerMemberDynamoDto {
+  public readonly pk: string;
+  public readonly sk: string;
+  public readonly gsi1Pk: string;
+  public readonly gsi1Sk: string;
+  private readonly _serverId: string;
+  private readonly _userId: string;
+  private readonly _username: string;
+  private _serverNickname?: string;
+  private _roleIds: string[];
+  private _isBanned: boolean;
+  private _isMuted: boolean;
+  private _isDeafened: boolean;
+
+  constructor(
+    serverId: string,
+    userId: string,
+    username: string,
+    serverNickname?: string,
+    roleIds: string[] = [],
+    isBanned: boolean = false,
+    isMuted: boolean = false,
+    isDeafened: boolean = false,
+  ) {
+    this._serverId = serverId;
+    this._userId = userId;
+    this._username = username;
+    this._serverNickname = serverNickname;
+    this._roleIds = roleIds;
+    this._isBanned = isBanned;
+    this._isMuted = isMuted;
+    this._isDeafened = isDeafened;
+    this.pk = ServerMemberDynamoDto.generatePk(this._serverId, this._userId);
+    this.sk = ServerMemberDynamoDto.generateSk(this._serverId);
+    this.gsi1Pk = ServerMemberDynamoDto.generateGsi1Pk(this._userId);
+    this.gsi1Sk = ServerMemberDynamoDto.generateGsi1Sk();
+  }
+
+  public static generatePk(serverId: string, userId: string) {
+    return `SERVER#${serverId}#MEMBER#${userId}`;
+  }
+
+  public static generateSk(serverId: string) {
+    return `SERVER#${serverId}#MEMBER`;
+  }
+
+  public static generateGsi1Pk(userId: string) {
+    return `SERVER_MEMBER#${userId}`;
+  }
+
+  public static generateGsi1Sk() {
+    return '';
+  }
+
+  public setServerNickname(serverNickname?: string) {
+    this._serverNickname = serverNickname;
+  }
+
+  public setBanned(isBanned: boolean) {
+    this._isBanned = isBanned;
+  }
+
+  public setMuted(isMuted: boolean) {
+    this._isMuted = isMuted;
+  }
+
+  public setDeafened(isDeafened: boolean) {
+    this._isDeafened = isDeafened;
+  }
+
+  public setRoles(roleIds: string[]) {
+    this._roleIds = roleIds;
+  }
+
+  public get username(): string {
+    return this._username;
+  }
+
+  public get serverNickname(): string | undefined {
+    return this._serverNickname;
+  }
+
+  public get roleIds(): string[] {
+    return this._roleIds;
+  }
+
+  public get isBanned(): boolean {
+    return this._isBanned;
+  }
+
+  public get isMuted(): boolean {
+    return this._isMuted;
+  }
+
+  public get isDeafened(): boolean {
+    return this._isDeafened;
+  }
+}
