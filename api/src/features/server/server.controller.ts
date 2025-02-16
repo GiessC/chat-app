59db0ca7f1d07bef9a7e18bac34cb41ae9f0f773
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-15 19:13:23 -0500

    added join server endpoint

--- api/src/features/server/server.controller.ts
+++ api/src/features/server/server.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common';
 import { ServerService } from './providers/server.service';
 import { CreateServerDto } from './dto/create-server.dto';
 import ApiResponse from '../../common/ApiResponse';
@@ -24,6 +31,26 @@ export class ServerController {
     };
     return new ApiResponse<ServerResponseDto>(
       `Created server '${createServerDto.name}'`,
+      serverResponseDto,
+    );
+  }
+
+  @Post('join')
+  @HttpCode(HttpStatus.OK)
+  public async join(
+    @Param('serverId') serverId: string,
+  ): Promise<ApiResponse<ServerResponseDto>> {
+    const userId = 'userId'; // TODO: Get userId from auth
+    const username = 'username'; // TODO: Get username from auth
+    const server = await this.serverService.join(serverId, userId, username);
+    const serverResponseDto: ServerResponseDto = {
+      serverId: server.serverId,
+      ownerId: server.ownerId,
+      name: server.name,
+      createdAt: server.createdAt,
+    };
+    return new ApiResponse<ServerResponseDto>(
+      `Joined server '${server.name}'`,
       serverResponseDto,
     );
   }
