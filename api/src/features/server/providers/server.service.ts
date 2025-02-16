50a48040b565ccce0fcd7a584a3102b9921229de
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-02-15 19:12:41 -0500

    added join method to server service

--- api/src/features/server/providers/server.service.ts
+++ api/src/features/server/providers/server.service.ts
@@ -1,18 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { ServerDynamoDbRepository } from './server.dynamo.repository';
 import { Server } from '../entities/server.entity';
+import { ServerMemberDynamoDbRepository } from './server-member.dynamo.repository';
+import { ServerMember } from '../entities/server-member.entity';
 
 @Injectable()
 export class ServerService {
-  constructor(private readonly repository: ServerDynamoDbRepository) {}
+  constructor(
+    private readonly serverRepo: ServerDynamoDbRepository,
+    private readonly serverMemberRepo: ServerMemberDynamoDbRepository,
+  ) {}
 
   public async create(ownerId: string, name: string) {
     try {
       const server = new Server(ownerId, name);
-      return await this.repository.create(server);
+      return await this.serverRepo.create(server);
     } catch (error: unknown) {
       console.error(`Server service error caused by: ${String(error)}`);
       throw new ServerServiceError('Failed to create server.');
+    }
+  }
+
+  async join(
+    serverId: string,
+    userId: string,
+    username: string,
+  ): Promise<Server> {
+    try {
+      const serverMember = new ServerMember(serverId, userId, username);
+      await this.serverMemberRepo.create(serverMember);
+      return await this.serverRepo.get(serverId);
+    } catch (error: unknown) {
+      console.error(`Server service error caused by: ${String(error)}`);
+      throw new ServerServiceError('Failed to join server.');
     }
   }
 }
