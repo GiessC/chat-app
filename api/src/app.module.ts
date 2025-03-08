622939df95b0eb23b0ac376c287d9a1b444582b9
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-03-08 13:50:10 -0500

    add cognito user pool registration

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ServerModule } from './features/server/server.module';
 import { ServerInviteModule } from './features/server-invite/server-invite.module';
+import { CognitoAuthModule } from '@nestjs-cognito/auth';
 
 @Module({
   imports: [
+    CognitoAuthModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        jwtVerifier: {
+          userPoolId: configService.get('COGNITO_USER_POOL_ID') ?? '',
+          clientId: configService.get('COGNITO_CLIENT_ID') ?? '',
+          tokenUse: 'id',
+        },
+      }),
+      inject: [ConfigService],
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [
