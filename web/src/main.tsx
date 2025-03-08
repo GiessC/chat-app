78ac65701c7b43769a350e93b954bb11a8b4a252
Collin Giess <68926012+GiessC@users.noreply.github.com>
2025-03-08 17:13:57 -0500

    add amplify configuration

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { Amplify } from 'aws-amplify';
+import { config } from './config/config.ts';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: config.USER_POOL_ID,
+      userPoolClientId: config.USER_POOL_CLIENT_ID,
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: 'code',
+      userAttributes: {
+        email: {
+          required: true
+        },
+        preferred_username: {
+          required: true,
+        }
+      },
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireNumbers: true,
+        requireUppercase: true,
+        requireSpecialCharacters: true,
+      }
+    }
+  }
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
